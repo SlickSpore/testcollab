0ad01074650f600549d1665ff868470358d61f44
progiulia <giulia.petrelli08@gmail.com>
2026-04-04 13:51:47 +0200

    Update giulia.py

--- giulia.py
+++ giulia.py
@@ -1,4 +1,46 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([2023, 2024, 2025, 2026])
+y1 = np.array([15, 25, 30, 20])
+y2 = np.array([17 , 23, 38, 5]) 
+y3 = np.array([13, 15, 20, 30])
+
+plt.title("Class size", 
+          fontsize=20,
+          family="Arial",
+          fontweight="bold",
+          color="blue"
+          )
+
+plt.xlabel("Year", 
+          fontsize=20,
+          family="Arial",
+          fontweight="bold",
+          color="blue"
+          )
+plt.ylabel("Students", 
+          fontsize=20,
+          family="Arial",
+          fontweight="bold",
+          color="blue"
+          )
 
 
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.plot(x, y3)
 
+plt.xticks(x)
+# line_style = dict(
+#             marker=".",
+#             markersize=20,
+#             markerfacecolor="cyan",
+#             linestyle="solid",
+#             linewidth=3)
+
+# plt.plot(x,y1, color="green", **line_style)
+# plt.plot(x, y2, color="red", **line_style)
+# plt.plot(x, y3, color="green", **line_style)
+
+plt.show()
